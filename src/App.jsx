38f34dbf945e044d73936735611cc79ea450ee26
Jang Yong Ho <96761280+JangKroed@users.noreply.github.com>
2025-03-30 23:47:23 +0900

    win square impact / draw

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react';
 
-function Square({ value, onSquareClick }) {
+function Square({ value, onSquareClick, winner }) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button
+      className={`square ${winner ? 'winner' : ''}`}
+      onClick={onSquareClick}
+    >
       {value}
     </button>
   );
 }
 
 function Board({ xIsNext, squares, onPlay }) {
+  const result = calculateWinner(squares);
   function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
+    if (result || squares[i]) {
       return;
     }
+
     const nextSquares = squares.slice();
 
     nextSquares[i] = xIsNext ? 'X' : 'O';
@@ -20,11 +25,12 @@ function Board({ xIsNext, squares, onPlay }) {
     onPlay(nextSquares);
   }
 
-  const winner = calculateWinner(squares);
   let status;
 
-  if (winner) {
-    status = 'Winner: ' + winner;
+  if (result?.winner) {
+    status = 'Winner: ' + result.winner;
+  } else if (result?.winner === 'Draw') {
+    status = 'Draw';
   } else {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
@@ -36,14 +42,20 @@ function Board({ xIsNext, squares, onPlay }) {
         if (i % 3 === 0) {
           return (
             <div className="board-row" key={i}>
-              <Square value={squares[i]} onSquareClick={() => handleClick(i)} />
+              <Square
+                value={squares[i]}
+                onSquareClick={() => handleClick(i)}
+                winner={result?.line?.includes(i)}
+              />
               <Square
                 value={squares[i + 1]}
                 onSquareClick={() => handleClick(i + 1)}
+                winner={result?.line?.includes(i + 1)}
               />
               <Square
                 value={squares[i + 2]}
                 onSquareClick={() => handleClick(i + 2)}
+                winner={result?.line?.includes(i + 2)}
               />
             </div>
           );
@@ -116,10 +128,14 @@ function calculateWinner(squares) {
     [2, 4, 6],
   ];
 
+  if (squares.every((square) => square !== null)) {
+    return { winner: 'Draw', line: [] };
+  }
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return { winner: squares[a], line: lines[i] };
     }
   }
 
