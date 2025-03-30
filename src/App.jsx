f75729ee5921f912f15513b99ac38e19e5c61d2a
Jang Yong Ho <96761280+JangKroed@users.noreply.github.com>
2025-03-30 23:25:36 +0900

    instead of a button/two loops?

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,15 @@ function Board({ xIsNext, squares, onPlay }) {
       return;
     }
     const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
-    }
+
+    nextSquares[i] = xIsNext ? 'X' : 'O';
+
     onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
   let status;
+
   if (winner) {
     status = 'Winner: ' + winner;
   } else {
@@ -33,21 +32,23 @@ function Board({ xIsNext, squares, onPlay }) {
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {squares.map((square, i) => {
+        if (i % 3 === 0) {
+          return (
+            <div className="board-row" key={i}>
+              <Square value={squares[i]} onSquareClick={() => handleClick(i)} />
+              <Square
+                value={squares[i + 1]}
+                onSquareClick={() => handleClick(i + 1)}
+              />
+              <Square
+                value={squares[i + 2]}
+                onSquareClick={() => handleClick(i + 2)}
+              />
+            </div>
+          );
+        }
+      })}
     </>
   );
 }
@@ -71,10 +72,11 @@ export default function Game() {
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description = 'Go to move #' + move;
+      description = `현재 ${move} 수를 진행 중입니다.`;
     } else {
       description = 'Go to game start';
     }
+
     return (
       <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
@@ -105,11 +107,13 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
+
   return null;
 }
